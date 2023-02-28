54eb316d17dc6d12529878db39795263d6053945
Lucas <lujogu@gmail.com>
2023-02-28 01:25:17 -0500

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import Logo from './assets/background.png';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h1}>GeneAI</Text>
+        <Text style={styles.h2}>A ONE-OF-A-KIND DATING EXPERIENCE.</Text>
+        <Image
+          source={Logo}
+          style={styles.image}
+        />
+        <View style={styles.buttonContainer}>
+          <Button
+          title="Press me"
+          color="#f194ff"
+          onPress={() => alert('Button with adjusted color pressed')}
+        />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+    backgroundColor: '#000',
     alignItems: 'center',
+    width: '100%',
+  },
+  h1: {
+    color: '#008F68',
+    fontSize: 40,
+  },
+  h2: {
+    color: '#FAE042',
+    fontSize: 18,
+    marginTop: 8,
+  },
+  image: {
+    width: 300,
+    height: 260,
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    backgroundColor: '#008F68',
+    borderRadius: 5,
+    padding: 8,
+    margin: 8,
   },
 });
