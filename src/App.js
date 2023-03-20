b6f4171a3e55baf85df1bfc7dd74a01564f2004f
Lucas <lujogu@gmail.com>
2023-03-20 12:34:23 -0400

    website shows things

--- src/App.js
+++ src/App.js
@@ -7,71 +7,71 @@ import Chat from './components/Chat-Page';
 import Profile from './components/Profile-Page';
 import Signup from './components/Signup-Page';
 import Legal from './components/Terms-Conditions-Page';
+import NavBar from './components/NavBar';
 
-import { Routes, Route, Outlet, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="Login" element={<Login />} />
-          <Route path="Match" element={<Match />} />
-          <Route path="Chat" element={<Chat />} />
-          <Route path="Profile Page" element={<Profile />} />
-          <Route path="Signup for GeneAI" element={<Signup />} />
-          <Route path="Legal Stuff" element={<Legal />} />
-          <Route path="*" element={<NoMatch />} />
+        <Route index element={<Home />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Match" element={<Match />} />
+        <Route path="Chat" element={<Chat />} />
+        <Route path="Profile Page" element={<Profile />} />
+        <Route path="Signup for GeneAI" element={<Signup />} />
+        <Route path="Legal Stuff" element={<Legal />} />
+        {/* <Route path="*" element={<NoMatch />} /> */}
         </Route>
       </Routes>
     </div>
   );
 }
 
-var navStyle = { textDecoration: "none", color: "red" };
+// Components/NavBar.js
+// import { NavLink } from 'react-router-dom';
 
 function Layout() {
-  return (
+ return (
+ <nav>
     <div>
-      <nav>
-        <ul>
+
+       <ul>
           <li>
-            <Link style={navStyle} to="/">Home</Link>
+            <NavLink to="/">Home</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Login-Page">Login</Link>
+            <NavLink to="/Login-Page">Login</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Matching-Page">Find Love</Link>
+            <NavLink to="/Matching-Page">Find Love</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Chat-Page">Chat</Link>
+            <NavLink to="/Chat-Page">Chat</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Profile-Page">User Profile</Link>
+            <NavLink to="/Profile-Page">User Profile</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Signup-Page">Join Now</Link>
+            <NavLink to="/Signup-Page">Join Now</NavLink>
           </li>
           <li>
-            <Link style={navStyle} to="/Terms-Conditions-Page">Legal Stuff</Link>
+            <NavLink to="/Terms-Conditions-Page">Legal Stuff</NavLink>
           </li>
         </ul>
-      </nav>
-
-      <hr />
-
-      <Outlet />
     </div>
-  );
-}
+ </nav>
+ );
+};
 
 function NoMatch() {
   return (
     <div>
       <h2>Whoopsies. You are lost!</h2>
-      <Link to="/">click here to return to the home page :)</Link>
+      <NavLink to="/">click here to return to the home page :)</NavLink>
     </div>
   );
 }
