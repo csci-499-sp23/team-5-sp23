b815a40b6758c9ed88a5c8c7b94c9a39c6d0f172
Michael Kim <michael.kim709@myhunter.cuny.edu>
2023-04-11 21:42:59 -0400

    Fixing security issue

--- src/components/API.js
+++ src/components/API.js
@@ -7,7 +7,7 @@ const options = {
 
 export const getMap = async () => {
   try {
-    const data = await fetch(`https://maps.googleapis.com/maps/api/js?key=AIzaSyCWVAQh0hgp61dBjz63ehaEEsZD9mUyFSY&libraries=places&callback=initMap`);
+    const data = await fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`);
     const res = data.json();
     console.log(res, "asdasdasdasdasdasa");
     console.log(data);
