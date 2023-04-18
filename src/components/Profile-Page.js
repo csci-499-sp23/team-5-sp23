75c4ef31116519c2f053707660d3f76db0875508
Michael Kim <michael.kim709@myhunter.cuny.edu>
2023-04-18 18:16:17 -0400

    Fixed Merging Errors

--- src/components/Profile-Page.js
+++ src/components/Profile-Page.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { TextField, Button } from "@material-ui/core";
+import { TextField, Button } from "@mui/material";
 
 const styles = {
     container: {
