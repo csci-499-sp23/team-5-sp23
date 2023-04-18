ebfb7f4349b325ecb2ddcdf098afb98226d69455
Amin <55928889+AminDevelopment@users.noreply.github.com>
2023-04-18 18:33:16 -0400

    Update Profile-Page.js
    Changing to  main

--- src/components/Profile-Page.js
+++ src/components/Profile-Page.js
@@ -1,96 +1,194 @@
-import './css/profileStyles.css';
-//import React, { useEffect, useState } from 'react';
-//import profilePicture from './img/profile_pic_guy.jpeg';
-//import {  doc, getDoc } from 'firebase/firestore'; //getDocs,  collection,
-//import { db } from '../firebase-config';
-//import { getStorage, ref, getDownloadURL } from 'firebase/storage';
-//import { UserAuth } from '../context/UserAuthContext';
+import React, { useState } from "react";
+import { TextField, Button } from "@mui/material";
 
-const Profile = () =>{
+const styles = {
+    container: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        background: "black",
+    },
+    textField: {
+        color: "white",
+        "& .MuiInputBase-input": {
+            textAlign: "center",
+        },
+    },
+    input: {
+        color: "white",
+        "& label": {
+            color: "white"
+        },
+        "& input": {
+            textAlign: "center"
+        }
+    },
+    color: "white"
+};
 
-    
-    //const [profile, setProfile] = useState([])
-    //const {user, logout} = UserAuth();
-   //const [profile_id, setID ] = useState("Add id")
-   //const [single_profile, setSingleProfile] = useState("")
-    //const profileCollectionRef = collection(db, 'profiles');
-    //const {logoutAccount} = UserAuth();
-    /*useEffect(() => {
-        getProfiles();
-    }, [])*/
+const Profile = () => {
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [birthdate, setBirthdate] = useState("");
+    const [bio, setBio] = useState("");
+    const [interests, setInterests] = useState("");
+    const [photos, setPhotos] = useState([]);
 
-    /*useEffect(() => {
-        console.log(profile)
-    }, [profile])*/
+    const handleNameChange = (event) => setName(event.target.value);
+    const handleLocationChange = (event) => setLocation(event.target.value);
+    const handleBirthdateChange = (event) => setBirthdate(event.target.value);
+    const handleBioChange = (event) => setBio(event.target.value);
+    const handleInterestsChange = (event) => setInterests(event.target.value);
 
-    //useEffect(() => {
-    //    console.log(single_profile)
-    //}, [single_profile])
-//
-    //const handleSubmit = (e) =>{
-    //    e.preventDefault();
-    //    getProfile();
-    //};
-//
-//
-    //const handleLogout = () => {
-//
-    //};
-//
-    //async function getProfile(){
-    //    console.log(profile_id)
-    //    const docRef = doc(db, "profiles", profile_id);
-    //    const docSnap = await getDoc(docRef);
-//
-    //    if (docSnap.exists()) {
-    //        console.log("Document data:", docSnap.data());
-    //      } else {
-    //        // doc.data() will be undefined in this case
-    //        console.log("No such document!");
-    //    }
-    //};
-    /*function getProfiles(){
-        
-        getDocs(profileCollectionRef)
-            .then(response => {
-                const prof = response.docs.map(doc => ({
-                    data: doc.data(),
-                    id: doc.id,
+    const handlePhotoUpload = (event) => {
+        const file = event.target.files[0];
+        if (file && photos.length < 6) {
+            setPhotos((prevPhotos) => [...prevPhotos, file]);
+        }
+    };
 
-                }))
-                setProfile(prof);
-            })
-            .catch(error => console.log(error.message))
+    const handleRemovePhoto = (index) => {
+        setPhotos((prevPhotos) => [
+            ...prevPhotos.slice(0, index),
+            ...prevPhotos.slice(index + 1),
+        ]);
+    };
 
-            /*
-            <div className="left_card" >
-                <h1>Left Hand side</h1>
-                <div>
-                    <img src={profilePicture} alt='Profile'/>
-                    <div className="bottom_div">
-                        This is where names and additional information goes
-                    </div>
-                </div>
-            </div>
-            <div className="right_card">
-                <h1>Right Side</h1>
-                <h1>Get Name</h1>
-                
-                <div>
-                    <p>Blur about user</p>
-                </div>
-                
-            </div>
-        }*/
-//<p>{profile_id}</p>
-//<button onClick={handleLogout}>Logout</button>
-        
-    return(
+    const handleSave = () => {
+        // enter database code here to save profile information
+        console.log({
+            name,
+            location,
+            birthdate,
+            bio,
+            interests,
+            photos,
+        });
+    };
+
+    return (
+        <div style={styles.container}>
         <div>
-            <h1>Hello</h1>
+            <h1>Profile</h1>
+            <div>
+                <input type="file" accept="image/*" onChange={handlePhotoUpload} />
+                {photos.map((photo, index) => (
+                    <div key={index}>
+                        <img src={URL.createObjectURL(photo)} alt="" />
+                        <button onClick={() => handleRemovePhoto(index)}>Remove</button>
+                    </div>
+                ))}
             </div>
-    );
-}
+            <div>
+                    <TextField
+                        label="Name"
+                        InputLabelProps={{
+                            style: {
+                                color: "white"
+                            }
+                        }}
+                        InputProps={{
+                            style: {
+                                color: "white"
+                            }
+                        }}
+                        value={name}
+                        onChange={handleNameChange}
+                    />
+            </div>
+            <div>
+                <TextField
+                    label="Location"
+                    value={location}
+                    onChange={handleLocationChange}
+                    InputLabelProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                />
+            </div>
+            <div>
+                <TextField
+                    label="Birthdate"
+                    type="date"
+                    value={birthdate}
+                    onChange={handleBirthdateChange}
+                    InputLabelProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                />
+            </div>
+            <div>
+                <TextField
+                    label="Bio"
+                    multiline
+                    rows={4}
+                    value={bio}
+                    onChange={handleBioChange}
+                    InputLabelProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                />
+            </div>
+            <div>
+                <TextField
+                    label="Interests"
+                    multiline
+                    rows={4}
+                    value={interests}
+                    onChange={handleInterestsChange}
+                    InputLabelProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                    InputProps={{
+                        style: {
+                            color: "white"
+                        }
+                    }}
+                />
+            </div>
+                <div>
+                    <Button variant="contained" color="primary" onClick={handleSave} style={{ color: 'white' }}>
+                        Save
+                    </Button>
+                </div>
+                <div>
+                    <Button variant="contained" color="secondary" style={{ color: 'white' }}>
+                        Dislike
+                    </Button>
+                    <Button variant="contained" color="primary" style={{ color: 'white' }}>
+                        Like
+                    </Button>
+                </div>
 
+        </div>
+        </div>
+    );
+};
 
 export default Profile;
+
+// button text is not coming out white...
