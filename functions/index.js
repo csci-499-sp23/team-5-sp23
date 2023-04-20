7297f305d6cee162eba9d5d4eec53e57d5fcf383
Michael Kim <michael.kim709@myhunter.cuny.edu>
2023-04-20 15:19:33 -0400

    4/19 Night Changes For Deployment

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,18 @@
-const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
+const functions = require("firebase-functions");
+// const cors = require('cors')({ origin: true });
 
-exports.firebaseGoogleAPI = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-    res.set('Access-Control-Allow-Origin', '*');
+// exports.firebaseGoogleAPI = functions.https.onRequest((req, res) => {
+//   cors(req, res, () => {
+//     res.set('Access-Control-Allow-Origin', '*');
 
-    const latitude = req.query.location.split(",")[0];
-    const longitude = req.query.location.split(",")[1];
-    const type = req.query.type;
-    const API_KEY = req.query.key;
+//     const latitude = req.query.location.split(",")[0];
+//     const longitude = req.query.location.split(",")[1];
+//     const type = req.query.type;
+//     const API_KEY = req.query.key;
 
-    fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&type=${type}&key=${API_KEY}`)
-      .then((response) => response.json())
-      .then((data) => res.send(data.results))
-      .catch((error) => console.error(error));
-  });
-});
+//     fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=5000&type=${type}&key=${API_KEY}`)
+//       .then((response) => response.json())
+//       .then((data) => res.send(data.results))
+//       .catch((error) => console.error(error));
+//   });
+// });
--- src/components/GoogleAPI.js
+++ src/components/GoogleAPI.js
@@ -1,49 +1,49 @@
 import React, { useState, useEffect } from "react";
-import axios from 'axios';
+import axios from "axios";
 // require('dotenv').config()
 
 function GoogleAPI() {
   const [places, setPlaces] = useState([]);
-  const category = { //may need to push out
+  const category = {
+    //may need to push out
     romantic: ["restaurant", "cafe", "park", "museum"],
     adventurous: ["zoo", "aquarium", "amusement_park"],
     cultural: ["art_gallery", "library", "theatre"],
     outdoor: ["beach", "hiking", "campground"],
   };
-  
+
   useEffect(() => {
-    
     const API_KEY = process.env.REACT_APP_API_KEY;
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      
+
       const promises = [];
-      
+
       for (const key in category) {
         const types = category[key].join("|");
-        const promise = axios.get(
-          `http://localhost:5001/csci499/us-central1/firebaseGoogleAPI?location=${latitude},${longitude}&radius=5000&type=${types}&key=${API_KEY}`
-          //`https://us-central1-geneai.cloudfunctions.net/firebaseGoogleAPI?location=${latitude},${longitude}&radius=5000&type=${types}&key=${API_KEY}`
+        const promise = axios
+          .get(
+            // `http://localhost:5001/csci499/us-central1/firebaseGoogleAPI?location=${latitude},${longitude}&radius=5000&type=${types}&key=${API_KEY}`
+            `https://us-central1-csci499.cloudfunctions.net/firebaseGoogleAPI?location=${latitude},${longitude}&radius=5000&type=${types}&key=${API_KEY}`
           )
           .then((response) => response.data)
           .catch((error) => console.error(error));
-          promises.push(promise);
-          
-          console.log("useEffect called"); //comments to make sure this runs properly
-        }
-        
-        Promise.all(promises).then((results) => {
-          const allPlaces = results.flat();
-          setPlaces(allPlaces);
-          console.log("Retrieved places:", allPlaces); //comments to make sure this runs properly
-        });
+        promises.push(promise);
+
+        console.log("useEffect called"); //comments to make sure this runs properly
+      }
+
+      Promise.all(promises).then((results) => {
+        const allPlaces = results.flat();
+        setPlaces(allPlaces);
+        console.log("Retrieved places:", allPlaces); //comments to make sure this runs properly
       });
-    }, []);
-    
-    
-    return (
-      <div>
+    });
+  }, []);
+
+  return (
+    <div>
       <h1>Date ideas near you:</h1>
       <ul>
         {places.map((place) => (
