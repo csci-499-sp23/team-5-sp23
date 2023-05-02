8894913aa11b04fbd6d86c994dc23f03e893f2c4
Michael Kim <michael.kim709@myhunter.cuny.edu>
2023-05-01 22:39:24 -0400

    Handled build warnings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import Home from './components/Home-Page';
-import Login from './components/Login-Page';
-import Match from './components/Matching-Page';
-import Chat from './components/Chat-Page';
-import Profile from './components/Profile-Page';
-import Signup from './components/Signup-Page';
-import TOS from './components/Terms-Conditions-Page';
-import NavBar from './components/NavBar';
-import Contact from './components/Contact-Page';
-import GoogleAPI from './components/GoogleAPI';
+import Home from "./components/Home-Page";
+import Login from "./components/Login-Page";
+import Match from "./components/Matching-Page";
+import Chat from "./components/Chat-Page";
+import Profile from "./components/Profile-Page";
+import Signup from "./components/Signup-Page";
+import TOS from "./components/Terms-Conditions-Page";
+import NavBar from "./components/NavBar";
+import Contact from "./components/Contact-Page";
 
 import { Routes, Route, NavLink } from "react-router-dom";
-import {AuthContextProvider} from './context/UserAuthContext'
-import ProtectedRoute from './components/ProtectedRoute';
+import { AuthContextProvider } from "./context/UserAuthContext";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 export default function App() {
   return (
@@ -19,18 +18,38 @@ export default function App() {
       <NavBar />
       <AuthContextProvider>
         <Routes>
-          <Route path="/" element={<Home />} /> 
+          <Route path="/" element={<Home />} />
           <Route path="/Login-Page" element={<Login />} />
-          <Route path="/Matching-Page" element={<ProtectedRoute><Match /></ProtectedRoute>} />
-          <Route path="/Chat-Page" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
-          <Route path="/Profile-Page" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+          <Route
+            path="/Matching-Page"
+            element={
+              <ProtectedRoute>
+                <Match />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Chat-Page"
+            element={
+              <ProtectedRoute>
+                <Chat />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Profile-Page"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/Signup-Page" element={<Signup />} />
           <Route path="/Terms-Conditions-Page" element={<TOS />} />
           <Route path="/Contact-Page" element={<Contact />} />
           <Route path="*" element={<NoMatch />} />
         </Routes>
       </AuthContextProvider>
-      
     </div>
   );
 }
