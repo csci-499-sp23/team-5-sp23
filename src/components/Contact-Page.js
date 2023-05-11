7ac44bd8e37dc421c712dd2c8d1ac8109a6a2033
La-Nique <54916166+La-Nique@users.noreply.github.com>
2023-05-11 01:02:53 -0400

    NavBar.js: corrected the link to the hidden contact page; ... Updated this on Contact-Page.js as well, we don't want a 404 page; ... Contact-Page.js: re-did contact page-- added in firebase configuration, which is WIP ; GoogleAPI.js: I am unable to see the API so I created a mock content to display what others can see so I can edit ; ... Signup/Login.js: fixed form length issue-- was related to static height size from prior ; Homepage.css: fixed padding that scrolled off page ; GoogleAPI.css: added in styling to destination bulletpoints ; Global-Styles.css: hid scroll for all pages to make site clean/hide any n00b ui design flaws--whoopies ; Contact-Page.css: completely new css file; added to the corresponding js file; aligned style to others

--- src/components/Contact-Page.js
+++ src/components/Contact-Page.js
@@ -1,30 +1,74 @@
 import { Link } from "react-router-dom";
 import logo from "./img/logo.png";
+import "./css/Contact-Page.css";
+import { useState } from 'react';
+import { db } from "../firebase-config";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { async } from "@firebase/util";
 
-const Contact = () => {
+// Technically, does not have to be linked to a user in db,
+// anyone on the web should be able to contact if they have questions
+
+function Contact() {
+
+  const[input, setInput] = useState("");
+
+  const handleInput = (e) => { setInput(e.target.value); }
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (input) {
+      await addDoc(collection(db, "contactUs"), {
+        email: input,
+        timestamp: serverTimestamp(),
+      })
+      setInput("");
+    }
+  }
+  
   return (
     <>
-    
-    <div className="logo-container">
-      <Link to="/">
-        <img src={logo} alt="persona logo" className="logo" />
-      </Link>
-    </div>
 
-    <form>
+      <div className="contactBody">
 
-      <h1>Contact Us!</h1>
+        <div className="logo-container">
+          <Link to="/">
+            <img src={logo} alt="persona logo" className="logo" />
+          </Link>
+        </div>
 
-      <input type="" name="name" id="" placeholder="Please enter your name!" />
-      <input type="email" name="email" id="" placeholder="Please enter your email!" />
-      <input type="phone" name="phone" id="" placeholder="Please enter your phone number!"/>
-      <textarea name="message" id="" cols="30" rows="10" placeholder="Please enter your message!"></textarea>
-      <button type="submit">send</button>
-    
-    </form>
+        <h1>Contact Us!</h1>
+
+        <form onSubmit={handleSubmit}>
+          <label>
+            Full Name:
+            <input onChange = {handleInput} type="text" name="name" id="name" value={input} />
+          </label>
+          <label>
+            Email:
+            <input onChange = {handleInput} type="email" name="email" id="email" />
+          </label>
+          <label>
+            Phone Number:
+            {/* to ensure compatibility, type must be "tel" since it is a phone number */}
+            <input onChange = {handleInput} type="tel" name="phone" id="phone" />
+          </label>
+          <textarea
+            onChange = {handleInput}
+            name="message"
+            id="message"
+            cols="30"
+            rows="10"
+            placeholder="Please Type Your Message Here!"
+          ></textarea>
+          <button type="submit">Send</button>
+        </form>
+
+      </div>
 
     </>
   );
-};
+}
 
 export default Contact;
